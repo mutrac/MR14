f37910beaba7d090a4fe34eb3a1f10752f4183ba
trevstanhope <tpstanhope@gmail.com>
2013-01-28 03:16:25 -0500

    added fuel gauge pin to 4

--- code/mr14/config.h
+++ code/mr14/config.h
@@ -1,6 +1,7 @@
 /* CONFIG FILE FOR MR14 */
 // PINS
-#define killSwitchPin 4
+#define killSwitchPin 3
+#define fuelGaugePin 4
 #define ignitionButtonPin 7 // pin for the Start Engine Button
 #define standbyRelayPin 5 // pin for the STANDBY mode
 #define ignitionRelayPin 6 // pin for relay to engine start
