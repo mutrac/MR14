55f8bf8b3a312dff9a5adcf6de35b317b81d5a8e
Trevor Stanhope <trevor@t510.(none)>
2013-05-22 11:05:04 -0400

    huge update to new LPH calculation method

--- code/TestFuelSensor/TestFuelSensor.ino
+++ code/TestFuelSensor/TestFuelSensor.ino
@@ -1,36 +1,39 @@
-// testFuelSensor.ino
-// Author: Trevor Stanhope
-// Updated: 2013-04-13
-// Test FS-3400AH Fuel Flow Sensor
+/*
+  TestFuelSensor
+  Author: Trevor Stanhope
+  Updated: 2013-04-13
+  Tests the FS-3400AH Fuel Flow Sensor
+*/
 
 /* --- Headers --- */
 #define BAUD 9600
-#define FUEL_PIN 3
+#define SENSOR_FUEL_PIN 3
 
 /* --- Declarations --- */
-volatile int NbTopsFan; //measuring the rising edges of the signal
-int Calc;                               
+volatile int SENSOR_FUEL; //measuring the rising edges of the signal
+volatile int INTERVAL;
+int RATE_FUEL;
 
 /* --- Setup --- */
 void setup() { 
-  pinMode(FUEL_PIN, INPUT); //initializes digital pin 2 as an input
+  pinMode(SENSOR_FUEL_PIN, INPUT); //initializes digital pin 2 as an input
   Serial.begin(BAUD); //This is the setup function where the serial port is initialised,
-  attachInterrupt(1, rpm, RISING); //and the interrupt is attached
+  attachInterrupt(1, fuel, RISING); //and the interrupt is attached
 } 
 
 /* --- Loop --- */
 void loop () {
-  NbTopsFan = 0; //Set NbTops to 0 ready for calculations
-  interrupts(); // Enables interrupts
-  delay(1000); // Wait 1 second
-  noInterrupts();
-  Calc = (NbTopsFan * 60 / 7.5); //(Pulse frequency x 60) / 7.5Q, = flow rate in L/hour 
-  Serial.print (Calc, DEC); //Prints the number calculated above
-  Serial.print ("L/hour\r\n"); //Prints "L/hour" and returns a  new line
+  SENSOR_FUEL = 0; //Set NbTops to 0 ready for calculations
+  INTERVAL = 0;
+  delay(1000);
+  INTERVAL += 1000; // Wait 1 second
+  RATE_FUEL = (SENSOR_FUEL * 1800) / INTERVAL;
+  Serial.print(RATE_FUEL, DEC); //P rints the number calculated above
+  Serial.print("L/hour\r\n"); // Prints "L/hour" and returns a  new line
 }
 
-/* --- RPM --- */
-void rpm () {    //This is the function that the interupt calls  
-  NbTopsFan++;  //This function measures the rising and falling edge of the hall effect sensors signal
+/* --- Fuel --- */
+void fuel() {    //This is the function that the interupt calls  
+  SENSOR_FUEL++;  //This function measures the rising and falling edge of the hall effect sensors signal
 } 
 
