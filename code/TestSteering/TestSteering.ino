5892940f01d99486de93eea79f1cf4867cdeebc3
Trevor Stanhope <trevor@t510.(none)>
2013-05-23 00:26:59 -0400

    updates to steering test

--- code/TestSteering/TestSteering.ino
+++ code/TestSteering/TestSteering.ino
@@ -12,28 +12,20 @@
 /* --- Declarations --- */
 #define STEERING_A_PIN  18
 #define STEERING_B_PIN  19
-#define LIMIT_FAR_POWER 34
-#define LIMIT_NEAR_PIN 35
-#define LIMIT_FAR_PIN 36
-#define LIMIT_FAR_PIN 37
 #define ACTUATOR_FAULT_PIN 6
 #define ACTUATOR_MEDIUM 50
 #define BAUD 9600
 DualVNH5019MotorShield motors; // M1 is steering actuator, M2 is ballast motor
 volatile int STEERING_POSITION = 0;
 volatile int ACTUATOR_POSITION = 0;
-int LIMIT_FAR = LOW;
-int LIMIT_NEAR = LOW;
 int ACTUATOR_FAULT = 0;
 
 /* --- Setup --- */
 void setup() { 
   
-  // Initialize I/O Pins
+  // Steering
   pinMode(STEERING_A_PIN, INPUT); 
   pinMode(STEERING_B_PIN, INPUT);
-  pinMode(LIMIT_NEAR_PIN, INPUT);
-  pinMode(LIMIT_FAR_PIN, INPUT);
   digitalWrite(STEERING_A_PIN, HIGH); // turn on pullup resistor
   digitalWrite(STEERING_B_PIN, HIGH); // turn on pullup resistor
 
@@ -50,34 +42,32 @@ void setup() {
 void loop(){
   Serial.println("----------");
   steering();
-  delay(1000);
+  delay(300);
 }
 
 /* --- Steering --- */
 void steering() {
   
   // Get actuator fault reading.
-  ACTUATOR_FAULT = digitalRead(ACTUATOR_FAULT_PIN);
+  //ACTUATOR_FAULT = digitalRead(ACTUATOR_FAULT_PIN);
+  ACTUATOR_FAULT = 0;
   
   // If actuator is stable, enable actuator.
   if (!ACTUATOR_FAULT) {
     
     // Until actuator is left of steering wheel, adjust right.
-    if (ACTUATOR_POSITION < STEERING_POSITION) {
+    while (ACTUATOR_POSITION < STEERING_POSITION) {
       motors.setM1Speed(ACTUATOR_MEDIUM);
       ACTUATOR_POSITION++;
     }
     
     // Until actuator is right of steering wheel, adjust left.
-    else if (ACTUATOR_POSITION > STEERING_POSITION) {
+    while (ACTUATOR_POSITION > STEERING_POSITION) {
       motors.setM1Speed(-ACTUATOR_MEDIUM);
       ACTUATOR_POSITION--;
     }
     
-    // Otherwise, disable actuator.
-    else {
-      motors.setM1Speed(0);
-    }
+    motors.setM1Speed(0);
   }
   
   // Otherwise, disable motor;
@@ -95,15 +85,9 @@ void encoder() {
   // Get the current encoder position and limit if oversteered.
   if (digitalRead(STEERING_A_PIN) == digitalRead(STEERING_B_PIN)) {
     STEERING_POSITION++;
-    if (STEERING_POSITION > 440) {
-      STEERING_POSITION = 440; // limit right steer to 440
-    }
   }
   else {
     STEERING_POSITION--;
-    if (STEERING_POSITION < -440) {
-    STEERING_POSITION = -440; // limit left steer to -440
-    }
   }
   Serial.println(STEERING_POSITION);
 }
