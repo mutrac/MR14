fb4dd5e417bb9bf00b6ee14e28a90ec298e94a28
Trevor Stanhope <trevor@t510.(none)>
2013-05-23 00:27:23 -0400

    finalized switch tester

--- code/TestSwitches/TestSwitches.ino
+++ code/TestSwitches/TestSwitches.ino
@@ -31,31 +31,52 @@ int LIMIT_NEAR = 0;
 int IGNITION = 0;
 
 void setup() {
-  // Power Pins
+  /* --- Switches --- */
+  // Button
   pinMode(KILL_BUTTON_POWER, OUTPUT);
   digitalWrite(KILL_BUTTON_POWER, LOW);
-  pinMode(KILL_SEAT_POWER, OUTPUT);
-  digitalWrite(KILL_SEAT_POWER, LOW);
-  pinMode(KILL_HITCH_POWER, OUTPUT);
-  digitalWrite(KILL_HITCH_POWER, LOW);
-  pinMode(LOCK_LEFTBRAKE_POWER, OUTPUT);
-  digitalWrite(LOCK_LEFTBRAKE_POWER, LOW);
-  pinMode(LOCK_RIGHTBRAKE_POWER, OUTPUT);
-  digitalWrite(LOCK_RIGHTBRAKE_POWER, LOW);
-  pinMode(IGNITION_POWER, OUTPUT);
-  digitalWrite(IGNITION_POWER, LOW);
-  
-  // Input Pins
   pinMode(KILL_BUTTON_PIN, INPUT);
   digitalWrite(KILL_BUTTON_PIN, HIGH);
+  
+  // Seat
+  pinMode(KILL_SEAT_POWER, OUTPUT);
+  digitalWrite(KILL_SEAT_POWER, LOW);
   pinMode(KILL_SEAT_PIN, INPUT);
+  digitalWrite(KILL_SEAT_PIN, HIGH);
+  
+  // Hitch
+  pinMode(KILL_HITCH_POWER, OUTPUT);
+  digitalWrite(KILL_HITCH_POWER, LOW);
+  pinMode(KILL_HITCH_PIN, INPUT);
+  digitalWrite(KILL_HITCH_PIN, HIGH);
+  
+  // Left Brake
+  pinMode(LOCK_LEFTBRAKE_POWER, OUTPUT);
+  digitalWrite(LOCK_LEFTBRAKE_POWER, LOW);
+  pinMode(LOCK_LEFTBRAKE_PIN, INPUT);
   digitalWrite(LOCK_LEFTBRAKE_PIN, HIGH);
+  
+  // Right Brake
+  pinMode(LOCK_RIGHTBRAKE_POWER, OUTPUT);
+  digitalWrite(LOCK_RIGHTBRAKE_POWER, LOW);
   pinMode(LOCK_RIGHTBRAKE_PIN, INPUT);
   digitalWrite(LOCK_RIGHTBRAKE_PIN, HIGH);
+  
+  // Ignition
+  pinMode(IGNITION_POWER, OUTPUT);
+  digitalWrite(IGNITION_POWER, LOW);
   pinMode(IGNITION_PIN, INPUT);
   digitalWrite(IGNITION_PIN, HIGH);
+  
+  // Near Limit
+  pinMode(LIMIT_NEAR_POWER, OUTPUT);
+  digitalWrite(LIMIT_NEAR_POWER, LOW);
   pinMode(LIMIT_NEAR_PIN, INPUT);
   digitalWrite(LIMIT_NEAR_PIN, HIGH);
+  
+  // Far Limit
+  pinMode(LIMIT_FAR_POWER, OUTPUT);
+  digitalWrite(LIMIT_FAR_POWER, LOW);
   pinMode(LIMIT_FAR_PIN, INPUT);
   digitalWrite(LIMIT_FAR_PIN, HIGH);
   
@@ -70,11 +91,14 @@ void loop() {
   KILL_SEAT = digitalRead(KILL_SEAT_PIN);
   LOCK_LEFTBRAKE = digitalRead(LOCK_LEFTBRAKE_PIN);
   LOCK_RIGHTBRAKE = digitalRead(LOCK_RIGHTBRAKE_PIN);
-  LIMIT_FAR = digitalRead(LIMIT_NEAR_PIN);
   LIMIT_NEAR = digitalRead(LIMIT_NEAR_PIN);
+  LIMIT_FAR = digitalRead(LIMIT_FAR_PIN);
   IGNITION = digitalRead(IGNITION_PIN);
   
   // Display values
+  Serial.println("---------------------");
+  Serial.print("Ignition: ");
+  Serial.println(IGNITION);
   Serial.print("Hitch: ");
   Serial.println(KILL_HITCH);
   Serial.print("Seat: ");
